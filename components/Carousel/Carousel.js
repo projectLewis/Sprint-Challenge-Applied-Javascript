8cc43f74379ce001369575d0193579a35c3c18c7
Seth Lewis <lewiscatenary@gmail.com>
2019-06-21 11:20:00 -0500

    stretch goal completed with functions

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,48 @@
-class Carousel {
+// class Carousel {
+//     constructor(carousel) {
+//         this.carousel = carousel;
+//     }
+//     nextImage() {
+//         const rightButton = document.querySelector('.right-button');
+//     }
+//     prevImage() {
+//         const leftButton = document.querySelector('.left-button');
+//     }
+// }
 
-}
+// let carousel = document.querySelector('.carousel');
+let carouselImages = document.querySelector('.carouselImgs');
+const firstImage = carouselImages.querySelector('img');
+firstImage.style.display = "block"
+const rightButton = document.querySelector('.right-button');
+const leftButton = document.querySelector('.left-button');
 
-let carousel = document.querySelector();
+let currentImage = firstImage;
+rightButton.addEventListener('click', () => {
+    currentImage.style.display = "none";
+    if (!currentImage.nextElementSibling) {
+        firstImage.style.display = "block";
+        currentImage = firstImage;
+    }
+    else {
+        currentImage.nextElementSibling.style.display = "block";
+        currentImage = currentImage.nextElementSibling;
+    }
+    return currentImage;
+});
+leftButton.addEventListener('click', () => {
+    currentImage.style.display = "none";
+    if (!currentImage.previousElementSibling) {
+        carouselImages.lastElementChild.style.display = "block";
+        // firstImage.style.display = "block";
+        currentImage = carouselImages.lastElementChild;
+    }
+    else {
+        currentImage.previousElementSibling.style.display = "block";
+        currentImage = currentImage.previousElementSibling;
+    }
+    return currentImage;
+});
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
