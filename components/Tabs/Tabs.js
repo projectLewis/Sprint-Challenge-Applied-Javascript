088fc3d0e96e900fe95db61467df98b0ce72917f
Seth Lewis <lewiscatenary@gmail.com>
2019-06-21 10:41:23 -0500

    finished select tab, and fixed assignment bug

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -15,12 +15,12 @@ class TabLink {
       this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelectorAll(`.card[data-tab="${ this.tabData }"]`);;
+      this.cards = document.querySelectorAll(`.card[data-tab="${ this.tabData }"]`);
     }
 
 
     // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    this.cards = this.cards.map((card) => {
+    this.cards = Array.from(this.cards).map((card) => {
       return new TabCard(card)
     });
 
@@ -33,22 +33,26 @@ class TabLink {
   selectTab() {
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab');
 
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+    tabs.forEach((tab) => {
+      tab.classList.remove('active-tab');
+    });
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach((card) => {
+      card.style.display = "none";
+    });
 
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+    this.tabElement.classList.add('active-tab');
 
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
 
@@ -61,8 +65,8 @@ class TabCard {
     // Update the style of this.cardElement to display = "flex"
     this.cardElement.style.display = "flex";
   }
-
 }
+
 // START HERE: 
 // - Select all classes named ".tab" and assign that value to the tabs variable
 
